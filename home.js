3bc31cc7299d9a7c71cfc2ccda1e368277e16778
kevinfan23 <kevinfan23@ucla.edu>
2016-04-08 20:50:07 -0700

    Version 1.0

---
+++ home.js
@@ -0,0 +1,65 @@
+$(document).ready( function(){
+    $.fn.snow();
+    $('.page-title').fadeIn(4500);
+    contactOverlay();
+});
+
+(function($){
+	
+	$.fn.snow = function(options){
+		var $flake 			= $('<div id="flake" />').css({'position': 'absolute', 'top': '-50px'}).html('&#8226;'),
+			documentHeight 	= $(document).height(),
+			documentWidth	= $(document).width(),
+			defaults		= {
+								minSize		: 8,
+								maxSize		: 10,
+								newOn		: 230,
+								flakeColor	: "#FFFFFF"
+							},
+			options			= $.extend({}, defaults, options);
+		
+		var interval		= setInterval( function(){
+			var startPositionLeft 	= Math.random() * documentWidth - 100,
+			 	startOpacity		= 0.5 + Math.random(),
+				sizeFlake			= options.minSize + Math.random() * options.maxSize,
+				endPositionTop		= documentHeight - 40,
+				endPositionLeft		= startPositionLeft - 100 + Math.random() * 200,
+				durationFall		= documentHeight * 10 + Math.random() * 5000;
+			$flake
+				.clone()
+				.appendTo('body')
+				.css(
+					{
+						left: startPositionLeft,
+						opacity: startOpacity,
+						'font-size': sizeFlake,
+						color: options.flakeColor
+					}
+				)
+				.animate(
+					{
+						top: endPositionTop,
+						left: endPositionLeft,
+						opacity: 0.2
+					},
+					durationFall,
+					'linear',
+					function() {
+						$(this).remove()
+					}
+				);
+		}, options.newOn);
+	
+	};
+	
+})(jQuery);
+
+function contactOverlay() {
+	$('#contact').on('click', function() {
+		$('#overlay-contact, .social').fadeIn(500);
+	});
+    
+	$(document).on('click','#overlay-contact',function() {
+        $('#overlay-contact, .social').fadeOut(300)
+    });
+}
